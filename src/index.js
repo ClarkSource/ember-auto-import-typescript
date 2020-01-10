d81e4dfe1f00f8ce3cac007025d5ddfdd37ea5d6
Jan Buschtöns <jan.buschtoens@clark.de>
2020-01-10 13:16:29 +0100

    fix: patch config instead of overriding

--- src/index.js
+++ src/index.js
@@ -10,7 +10,44 @@ module.exports = {
   },
 
   patchConfig(parent) {
+    // Create `parent.options.autoImport.webpack = {}`
     if (!parent.options) parent.options = {};
-    parent.options.autoImport = require('./config')(this.project.targets);
+    if (!parent.options.autoImport) parent.options.autoImport = {};
+    const { autoImport } = parent.options;
+    if (!autoImport.webpack) autoImport.webpack = {};
+    const { webpack } = autoImport;
+
+    // Unshift `.ts` extension to `webpack.resolve.extensions`
+    if (!webpack.resolve) webpack.resolve = { extensions: ['.ts', '.js'] };
+    else if (!webpack.resolve.extensions)
+      webpack.resolve.extensions = ['.ts', '.js'];
+    else if (!webpack.resolve.extensions.includes('.ts'))
+      webpack.resolve.extensions.unshift('.ts');
+
+    const tsModuleRule = {
+      test: /\.ts$/,
+      use: {
+        loader: 'babel-loader-8',
+        options: {
+          presets: [
+            // Transpile TypeScript
+            require.resolve('@babel/preset-typescript'),
+            // Transpile incompatible syntax for project build targets
+            [
+              require.resolve('@babel/preset-env'),
+              {
+                modules: false,
+                targets: this.project.targets
+              }
+            ]
+          ]
+        }
+      }
+    };
+
+    // Push `tsModuleRule` into `webpack.module.rules`
+    if (!webpack.module) webpack.module = { rules: [tsModuleRule] };
+    else if (!webpack.module.rules) webpack.module.rules = [tsModuleRule];
+    else webpack.module.rules.push(tsModuleRule);
   }
 };
