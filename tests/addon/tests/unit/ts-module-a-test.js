b1f9a116059e8c6accd7ebb2d2b1c8b1e0a816b3
Jan Buschtöns <jan.buschtoens@clark.de>
2020-10-14 18:34:36 +0200

    test: fix enum names

--- tests/addon/tests/unit/ts-module-a-test.js
+++ tests/addon/tests/unit/ts-module-a-test.js
@@ -5,8 +5,8 @@ import { works, SomeEnum, moduleB } from '@test/addon';
 module('ts-module-a', function () {
   test('it works', function (assert) {
     assert.ok(works());
-    assert.ok('foo' in SomeEnum);
-    assert.ok('bar' in SomeEnum);
+    assert.ok('Foo' in SomeEnum);
+    assert.ok('Bar' in SomeEnum);
     assert.ok(moduleB.nestedWorks());
   });
 });
--- tests/app/tests/unit/ts-module-a-test.js
+++ tests/app/tests/unit/ts-module-a-test.js
@@ -5,8 +5,8 @@ import { works, SomeEnum, moduleB } from '@test/ts-module-a';
 module('ts-module-a', function () {
   test('it works', function (assert) {
     assert.ok(works());
-    assert.ok('foo' in SomeEnum);
-    assert.ok('bar' in SomeEnum);
+    assert.ok('Foo' in SomeEnum);
+    assert.ok('Bar' in SomeEnum);
     assert.ok(moduleB.nestedWorks());
   });
 });
